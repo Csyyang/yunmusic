b93541e07ed8a9800a465d255edaea04898f8f93
csy23333 <693765678@qq.com>
2021-09-17 17:08:33 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// #ifndef VUE3
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+let fontSize 
+let screenWidth = uni.getSystemInfoSync().screenWidth
+screenWidth = screenWidth >750?'750':screenWidth
+fontSize = 100 * (screenWidth / 375) + 'px'
+console.log(screenWidth)
+Vue.prototype.$rootFontSize = fontSize
+
+
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
---
+++ pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<page-meta :root-font-size="rootFontSize"></page-meta>
+	<view id="container">
+		<view class="search-header">
+			<uni-icons class="icons" type="list" size="22"></uni-icons>
+			<uni-search-bar class="search" v-model="searchInput" @input="search" cancelButton="none"
+				placeholder="请输入歌曲名/歌手名" />
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				searchInput: '',
+				rootFontSize: this.$rootFontSize
+			}
+		},
+		methods: {
+			search: function(e) {
+				console.log(this.searchInput)
+			}
+		}
+	}
+</script>
+
+
+<style lang="scss" scoped>
+	.search-header {
+		background-color: #d44439;
+		display: flex;
+		height: 44px;
+		align-items: center;
+
+		.icons {
+			padding: 11px;
+		}
+
+		.search {
+
+			/deep/ .uni-searchbar__box {
+				height: 22px;
+			}
+		}
+	}
+</style>
